6f876040b889c5099d5c3d02815dfb02466a7d75
Nawfal RAZOUK <nawfalrazouk7@gmail.com>
2024-05-25 17:47:47 +0100

    Task 1

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -11,9 +11,9 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/', function (req, res) {
+  // Return the list of books as JSON
+  return res.status(200).json({ books: books });
 });
 
 // Get book details based on ISBN
